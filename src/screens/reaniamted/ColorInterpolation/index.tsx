e27cf0fe0df64a7203bd726e1c59edf16b6ddfbf
Sumanth Yeodti <sumanth.yedoti@gmail.com>
2022-12-02 00:14:12 +0530

    Add infinite rotation to the react icon

--- src/screens/reaniamted/ColorInterpolation/index.tsx
+++ src/screens/reaniamted/ColorInterpolation/index.tsx
@@ -1,7 +1,8 @@
 /*
-  includes: svg animation
+  - svg animation
+  - infinite animation
 */
-import {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Svg, {Path} from 'react-native-svg'
 import {Dimensions, StyleSheet, Switch} from 'react-native'
 import Animated, {
@@ -11,7 +12,10 @@ import Animated, {
   useDerivedValue,
   withTiming,
   interpolate,
+  useSharedValue,
+  Easing,
 } from 'react-native-reanimated'
+import {ThemeProvider} from '@react-navigation/native'
 
 const {width, height} = Dimensions.get('window')
 const CIRCLE_SIZE = width / 3.6
@@ -26,7 +30,7 @@ enum Theme {
 
 const colors = {
   [Theme.DARK]: {
-    bgPrimary: '#1e1e1e',
+    bgPrimary: '#050505',
     bgSecondary: '#333',
     fgPrimary: '#f0f0f0',
   },
@@ -76,12 +80,31 @@ const ColorInterpolation: React.FC = () => {
     }
   })
 
+  let degrees = useSharedValue(0)
+  const rotationTimingOptions = {
+    duration: 4000,
+    easing: Easing.linear,
+  }
+  useEffect(() => {
+    if (theme === Theme.DARK) {
+      degrees.value = withRepeat(
+        withTiming(degrees.value + 360, rotationTimingOptions),
+        -1,
+        false,
+      )
+    } else {
+      degrees.value = withRepeat(
+        withTiming(degrees.value - 360, rotationTimingOptions),
+        -1,
+        false,
+      )
+    }
+  }, [theme])
   const rIconViewStyles = useAnimatedStyle(() => {
-    const degrees = interpolate(isDark01.value, [1, 0], [90, 0])
     return {
       transform: [
         {
-          rotate: degrees + 'deg',
+          rotate: degrees.value + 'deg',
         },
       ],
     }
@@ -89,7 +112,7 @@ const ColorInterpolation: React.FC = () => {
 
   return (
     <Animated.View style={[styles.container, rContainerStyles]}>
-      <Animated.View style={[rIconViewStyles]}>
+      <Animated.View style={[styles.iconContainer, rIconViewStyles]}>
         <Svg width={ICON_SIZE} height={ICON_SIZE} viewBox="0 0 512 512">
           <AnimatedPath
             animatedProps={rIconStyles}
@@ -118,6 +141,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  iconContainer: {
+    position: 'absolute',
   },
   circle: {
     position: 'absolute',
